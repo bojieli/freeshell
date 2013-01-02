f8b0b33e14d9036f54e4cb9d237ae1a2a0e88e30
Bojie Li <bojieli@gmail.com>
2013-01-02 22:25:34 +0800

    fix install

--- conf/install.sql
+++ conf/install.sql
@@ -3,7 +3,7 @@ CREATE TABLE shellinfo (
     `id` INT(10) NOT NULL AUTO_INCREMENT,
     `nodeno` INT(10) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(100) NOT NULL, --salted
+    `password` VARCHAR(100) NOT NULL,
     `hostname` VARCHAR(255) NOT NULL,
     `isactive` TINYINT(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`),
