af251dde59910b28d40cb3ec4869094482968e8f
Bojie Li <bojieli@gmail.com>
2013-01-08 15:50:26 +0800

    runinallvz.sh

---
+++ conf/runinallvz.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# usage: ./execinallvz.sh <command>
+
+if [ -z $1 ]; then
+    echo "usage: ./execinallvz.sh <command>";
+    exit 1
+fi
+command=$@
+
+max=$(curl http://blog.ustc.edu.cn/freeshell/shellmax.php)
+for id in $(seq 101 $max); do
+    host=$(echo $id % 7 | bc)
+    if [ $host -eq 0 ]; then
+        host=7
+    fi
+    echo $host:$id
+    ssh scgyshell-$host "sudo vzctl enter $id --exec $command"
+done
