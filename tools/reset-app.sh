194d9b7decc977cf7f18109c2c28e89e520f8345
Bojie Li <bojieli@gmail.com>
2013-05-01 21:49:17 +0800

    finish reset-app

--- tools/reset-app.sh
+++ tools/reset-app.sh
@@ -4,21 +4,32 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-host=$(./getipbyappid $1)
-if [ -z $host ]; then
-    echo "Unknown appid $1"
+appid=$1
+hostip=$(./getipbyappid $appid)
+node=$(./getnodebyappid $appid)
+if [ -z $node ]; then
+    echo "Unknown appid $appid"
     exit 1
 fi
-appid=$1
+host="s${node}.freeshell.ustc.edu.cn"
 
 hostname=$(ssh $host "sudo vzlist -H -o hostname $appid")
-if [ -z $hostname]; then
+if [ -z $hostname ]; then
     echo "Unknown hostname"
     exit 1
 fi
 
-ssh $host "sudo vzctl stop $id"
-ssh $host "sudo vzctl destroy $id"
-passwd=$(openssl rand -base64 9)
+read -p "Are you sure to reset freeshell $appid ($hostname)? [y/n] "
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+fi
+
+ssh $host "sudo vzctl stop $appid"
+ssh $host "sudo vzctl destroy $appid"
+passwd=$(openssl rand -base64 9 | tr -d '\n\r')
 ssh $host "sudo /home/boj/scripts/create-vz.sh $appid $hostname $passwd"
-ssh $host "sudo /home/boj/scripts/activate-vz.sh $appid $host"
+ssh $host "sudo /home/boj/scripts/activate-vz.sh $appid $hostip"
+
+echo
+echo "===== FREESHELL-ADMIN ====="
+echo "Done. New password is: $passwd"
