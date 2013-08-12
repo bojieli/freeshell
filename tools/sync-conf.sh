24502409ecf622ffa005192b31b69bdfcf77bbf0
Bojie Li <bojieli@gmail.com>
2013-08-12 23:37:05 +0800

    must ensure sudoers perm is OK before overwritting

--- tools/sync-conf.sh
+++ tools/sync-conf.sh
@@ -9,15 +9,16 @@ while read host; do
 done <pssh-hosts
 
 function prun() {
-    parallel-ssh -h pssh-hosts "sudo $1"
+    parallel-ssh -h pssh-hosts "$1"
 }
 
 # preserve perms and execution bit, chown as root
-prun "rsync -rpE $BASE/etc/ /etc/"
-prun "chmod 440 /etc/sudoers"
+# must ensure perm of sudoers is OK
+prun "chmod 440 $BASE/etc/sudoers; \
+    sudo rsync -rpE $BASE/etc/ /etc/"
 
 KEY_SRC="$BASE/scgyshell-client.authorized_keys"
 KEY_TARGET="/home/scgyshell-client/.ssh/authorized_keys"
-prun "chown scgyshell-client:scgyshell-client $KEY_SRC"
-prun "chmod 600 $KEY_SRC"
-prun "mv $KEY_SRC $KEY_TARGET"
+prun "sudo chown scgyshell-client:scgyshell-client $KEY_SRC; \
+    sudo chmod 600 $KEY_SRC; \
+    sudo mv $KEY_SRC $KEY_TARGET"
