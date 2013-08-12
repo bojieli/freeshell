e84438fa7e7122c3ac0fc694abb170f4a3466b4f
Bojie Li <bojieli@gmail.com>
2013-08-12 23:59:50 +0800

    make sudoers check more stronger

---
+++ conf/update-conf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# update configs from this dir to system dir
+
+BASE="/home/boj/scripts"
+
+if [ `whoami` != "root" ]; then
+    echo "You are not root!"
+    exit 1
+fi
+
+# preserve perms and execution bit, chown as root
+rsync -rpE --exclude=sudoers $BASE/etc/ /etc/
+
+KEY_SRC="$BASE/scgyshell-client.authorized_keys"
+KEY_TARGET="/home/scgyshell-client/.ssh/authorized_keys"
+chown scgyshell-client:scgyshell-client $KEY_SRC
+chmod 600 $KEY_SRC
+mv $KEY_SRC $KEY_TARGET
+
+# must ensure perm of sudoers is OK
+chmod 440 $BASE/node-sudoers
+visudo -c -f $BASE/node-sudoers
+if [ "$?" -eq 0 ]; then
+    mv $BASE/node-sudoers /etc/sudoers
+    exit 0
+else
+    exit 1
+fi
--- tools/sync-conf.sh
+++ tools/sync-conf.sh
@@ -2,23 +2,11 @@
 
 cd `dirname $0`
 CONFDIR="../conf"
-BASE="/home/boj/scripts"
 
+echo "Uploading files..."
 while read host; do
     rsync -a --delete $CONFDIR/ $host:/home/boj/scripts/    
 done <pssh-hosts
 
-function prun() {
-    parallel-ssh -h pssh-hosts "$1"
-}
-
-# preserve perms and execution bit, chown as root
-# must ensure perm of sudoers is OK
-prun "chmod 440 $BASE/etc/sudoers; \
-    sudo rsync -rpE $BASE/etc/ /etc/"
-
-KEY_SRC="$BASE/scgyshell-client.authorized_keys"
-KEY_TARGET="/home/scgyshell-client/.ssh/authorized_keys"
-prun "sudo chown scgyshell-client:scgyshell-client $KEY_SRC; \
-    sudo chmod 600 $KEY_SRC; \
-    sudo mv $KEY_SRC $KEY_TARGET"
+echo "Updating configs..."
+parallel-ssh -h pssh-hosts "sudo /home/boj/scripts/update-conf.sh"
