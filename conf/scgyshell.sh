81a18db52cf180e498c6092747adf09eeceff11d
Bojie Li <bojieli@gmail.com>
2013-08-12 22:42:59 +0800

    scgyshell client script use input as param

--- conf/scgyshell.sh
+++ conf/scgyshell.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-# usage: scgyshell.sh <action> <params>
+# usage: input <action> <params> in one line to stdin
 
-if [ -z $1 ]; then
+# This script has no security check.
+# Please ensure the input does not have injections.
+
+read action params;
+if [ -z "$action" ]; then
     echo "Welcome to scgyshell!"
     exit 1;
 fi
-action=$1;
 
 if [ -f `dirname $0`/${action}.sh ]; then
-    `dirname $0`/${action}.sh $(echo $@ | awk '{$1="";print $0}')
+    `dirname $0`/${action}.sh $params
+elif [ "$action" == "vzlist" ]; then
+    sudo vzlist $params
 else
     `dirname $0`/control-vz.sh $@
 fi
