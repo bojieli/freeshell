435f9a4a83cc7971b1e379aaa3d401750e34549b
Bojie Li <bojieli@gmail.com>
2013-08-13 16:04:20 +0800

    fix status.php
    escape quotes in ssh command

--- http/status.php
+++ http/status.php
@@ -1,5 +1,5 @@
 <?php
-include_once "header.php";
+session_start();
 include_once "db.php";
 include_once "nodes.inc.php";
 
@@ -52,59 +52,13 @@ $detail_attrs = array(
     'bootorder',
 );
 
-$cmd = "sudo vzlist -o ";
+$cmd = "vzlist -o ";
 if (isset($_GET['detail']))
     $cmd .= implode(',', $detail_attrs);
 else
     $cmd .= implode(',', $attrs);
 ?>
-<div id="wrapper">
-<div id="regtitle">
-        	<h1>Freeshell System Status</h1>
-        	<div id="progbar">
-            </div>
-<style>
-ul.table, ul.help, p.note {
-	font-size: 16px !important;
-	font-family: "Segoe UI","Helvetica Neue", Helvetica, Ubuntu;
-	margin-left: 100px !important;
-	margin-top: 20px;
-	line-height: 20px;
-}
-h2 {
-	font-size: 20px !important;
-	font-family: "Segoe UI","Helvetica Neue", Helvetica, Ubuntu;
-	font-weight: 100;
-	margin-left: 100px !important;
-}
-ul.table span.h {
-    display: inline-block;
-    width: 200px;
-    margin-right: 30px;
-}
-ul.table span.r {
-    display: inline-block;
-}
-ul.table span.c {
-    display: inline-block;
-    font-family: "Courier New", "Monospace";
-    font-size: 14px;
-}
-ul.table li {
-    margin: 5px 0 5px 0;
-}
-ul.help li {
-    list-style: square;
-    margin: 10px 0 10px 0;
-    width: 700px;
-}
-.buttons span {
-    margin-right: 30px;
-}
-p.note {
-    width: 700px;
-}
-</style>
+<h1>Freeshell System Status</h1>
 <?php
 foreach ($nodes2ip as $nodeno => $ip) {
     echo "<h2>Node #$nodeno: $ip</h2>\n";
@@ -113,6 +67,3 @@ foreach ($nodes2ip as $nodeno => $ip) {
     echo "</pre>\n";
 }
 ?>
-</div>
-</div>
-<script src="js/jquery.js" type="text/javascript"></script>
