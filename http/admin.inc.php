78664e12c174168e7f058a646820a87f14633177
Bojie Li <bojieli@gmail.com>
2013-09-19 01:13:49 +0800

    fix account activation letter

--- http/admin.inc.php
+++ http/admin.inc.php
@@ -15,7 +15,7 @@ function error($msg) {
 
 function send_activate_mail($email, $appid, $token) {
     $title = "Account Activation for USTC freeshell";
-    $body = "Hello,\n\nThanks for being the ".($appid-100)."-th user of USTC freeshell. Please click on the link below (or copy it to the address bar) to activate your blog account.\n\nhttp://blog.ustc.edu.cn/freeshell/activate.php?appid=$appid&token=$token\n\nThis link will expire in 48 hours. Any problems, please email us: lug@ustc.edu.cn\n\nSincerely,\nUSTC Freeshell Team";
+    $body = "Hello,\n\nThanks for being the ".($appid-100)."-th user of USTC freeshell. Please click on the link below (or copy it to the address bar) to activate your freeshell account.\n\nhttp://freeshell.ustc.edu.cn/activate.php?appid=$appid&token=$token\n\nThis link will expire in 48 hours. Any problems, please email us: lug@ustc.edu.cn\n\nSincerely,\nUSTC Freeshell Team";
     $headers = 'From: noreply@blog.ustc.edu.cn';
     mail($email, $title, $body, $headers);
 }
