5f45d3f5cda2a1773465bb055643754e825265ce
Bojie Li <bojieli@gmail.com>
2013-11-03 15:58:47 +0800

    check file existence and change to curr directory in file upload util

--- tools/upload.sh
+++ tools/upload.sh
@@ -5,9 +5,13 @@ if [ -z $1 ] || [ -z $2 ]; then
     echo "usage: ./upload.sh <filename> <remote-path>";
     exit 1
 fi
+if [ ! -e "$1" ]; then
+    echo "File or directory not exist"
+    exit 1
+fi
 
 echo "Uploading $1 ..."
-parallel-scp -h pssh-hosts $1 /home/boj
+parallel-scp -r -h $(dirname $0)/pssh-hosts $1 /home/boj
 echo "Moving $1 to $2 ..."
-parallel-ssh -h pssh-hosts "sudo mv /home/boj/$1 $2"
+parallel-ssh -h $(dirname $0)/pssh-hosts "sudo mv /home/boj/$1 $2"
 exit 0
