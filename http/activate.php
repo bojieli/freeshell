bd4dbd9d2a33da71332054e2ee9fa445153a94df
Bojie Li <bojieli@gmail.com>
2013-11-03 16:17:50 +0800

    account activate page: IPv6 instead of IPv4; display page after activation

--- http/activate.php
+++ http/activate.php
@@ -17,6 +17,8 @@ if ($info['isactive']) {
 if ($info['token'] !== $_GET['token'])
     die('Incorrect token. Please copy the link to the address bar of your browser and retry.');
 
+activate_vz($info['nodeno'], $appid);
+
 mysql_query("UPDATE shellinfo SET `isactive`=1 WHERE `id`='$appid'");
 
 // auto login
@@ -30,7 +32,7 @@ $_SESSION['appid'] = $info['id'];
             </div>
 <p>All things done.</p>
 <p>You can login to your shell with SSH now:</p>
-<p style="font:Courier New">ssh -p <?php echo $appid + 10000 ?> root@<?php echo get_node_ip(1); ?></p>
+<p style="font:Courier New">ssh <?php echo get_node_ipv6($appid) ?></p>
 <p>The root password is the same as the login password of control panel.</p>
 <p>It is recommended that you create your own account and login with it instead of root.</p>
 <p>Also you can login to the control panel for more info:</p>
@@ -39,7 +41,5 @@ $_SESSION['appid'] = $info['id'];
         	<p>Let's Rock!</p>
 </div>
 </div>
-<?php
-fastcgi_finish_request();
-activate_vz($info['nodeno'], $appid);
-?>
+</body>
+</html>
