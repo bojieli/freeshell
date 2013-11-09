765e8e53b5c269777868e05af8016e7864e7962f
Bojie Li <bojieli@gmail.com>
2013-11-09 18:44:46 +0800

    make nat-entry-node more robust

--- conf/nat-entry-node.sh
+++ conf/nat-entry-node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# usage: ./nat-entry-node.sh <id> <localip> <nodeip>
+# usage: ./nat-entry-node.sh <id> <localip> <nodeip> [renew]
 
 if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
     exit 1
@@ -13,6 +13,13 @@ httpport=$(echo $id + 20000 | bc)
 
 if [ $localip == $nodeip ]; then
 	exit 1
+fi
+
+if [ "$4" == "renew" ]; then
+    iptables -t nat -D PREROUTING -i eth0 -p tcp --dport $sshport -j DNAT --to-destination $nodeip:$sshport
+    iptables -t nat -D POSTROUTING -p tcp --dport $sshport -j SNAT --to-source $localip
+    iptables -t nat -D PREROUTING -i eth0 -p tcp --dport $httpport -j DNAT --to-destination $nodeip:$httpport
+    iptables -t nat -D POSTROUTING -p tcp --dport $httpport -j SNAT --to-source $localip
 fi
 
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport $sshport -j DNAT --to-destination $nodeip:$sshport
