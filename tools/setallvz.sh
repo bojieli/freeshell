57b1b0664e5375764e54432042943238a39dc713
Bojie Li <bojieli@gmail.com>
2013-11-15 22:19:38 +0800

    use FQDN of freeshell nodes

--- tools/setallvz.sh
+++ tools/setallvz.sh
@@ -2,14 +2,14 @@
 # usage: ./setallvz.sh <option> <value>
 # if need appid in value, use $id
 
-if [ -z $1 ] || [ -z $2 ]; then
+if [ -z "$1" ] || [ -z "$2" ]; then
     echo "usage: ./setallvz.sh <option> <value>";
     exit 1
 fi
 option=$1
 value=$2
 
-max=$(curl http://blog.ustc.edu.cn/freeshell/shellmax.php)
+max=$(curl https://blog.ustc.edu.cn/freeshell/shellmax.php)
 for id in $(seq 101 $max); do
     host=$(echo $id % 7 | bc)
     if [ $host -eq 0 ]; then
@@ -17,5 +17,5 @@ for id in $(seq 101 $max); do
     fi
     echo $host:$id
     realvalue=$(echo $value | sed "s/\$id/$id/g")
-    ssh scgyshell-$host "sudo vzctl set $id --$option $realvalue --save"
+    ssh s$host.freeshell.ustc.edu.cn "sudo vzctl set $id --$option $realvalue --save"
 done
