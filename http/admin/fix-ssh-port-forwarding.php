a51f8b693006e1c6b7a9f02469e9b30fd6839ed3
Bojie Li <bojieli@gmail.com>
2014-03-24 19:31:59 +0800

    utils to fix ssh port forwarding

---
+++ http/admin/fix-ssh-port-forwarding.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+if (empty($_SESSION['isadmin']))
+    exit();
+include_once '../db.php';
+include_once '../nodes.inc.php';
+
+$rs = mysql_query("SELECT id, nodeno FROM shellinfo WHERE isactive=1");
+while ($row = mysql_fetch_array($rs)) {
+    add_ssh_port_forwarding($row['id'], $row['nodeno']);
+}
+echo "Done.";
