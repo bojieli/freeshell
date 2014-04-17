ab6c39a79154d9676bf7c37278e6b24317e9f7fe
Bojie Li <bojieli@gmail.com>
2014-04-17 21:51:07 +0800

    croncheck locks in freeshell

--- http/admin/croncheck.php
+++ http/admin/croncheck.php
@@ -3,6 +3,7 @@
 
 date_default_timezone_set('Asia/Chongqing');
 include_once "../nodes.inc.php";
+include_once "../db.php";
 
 foreach ($nodes2ip as $nodeno => $ip) {
     global $errno;
@@ -12,6 +13,19 @@ foreach ($nodes2ip as $nodeno => $ip) {
             "Return Code: $errno\n$response");
     }
 }
+
+// check freeshells that have been locked for more than 1 hour
+$time_threshold = intval(time() - 3600);
+$locked_shell_count = mysql_result(checked_mysql_query("SELECT COUNT(*) FROM shellinfo WHERE locked=1 AND lock_time<$time_threshold"), 0);
+if ($locked_shell_count != 0) {
+    $rs = mysql_query("SELECT id, email, lock_time FROM shellinfo WHERE locked=1 AND lock_time<$time_threshold");
+    $msg = "Locked shell count: $locked_shell_count\n";
+    while ($row = mysql_fetch_array($rs)) {
+        $msg .= "id=".$row['id']." email=".$row['email']." locked since ".date("Y-m-d H:i:s", $row['lock_time'])."\n";
+    }
+    report_sys_admin($msg);
+}
+
 function mail_warning($email, $nodeno, $action, $detail = "") {
     $title = "Freeshell node $nodeno $action failed";
     $body = "This email is for freeshell system admin.\n\nCroncheck target $action failed for node $nodeno.\n\nDetail: $detail\n\nDate: ".date("Y-m-d H:i:s");
