6211d73fa91f4db0ebcda9e7b01ddd78f610e929
Bojie Li <bojieli@gmail.com>
2014-04-17 17:30:02 +0800

    check and report mysql query errors

--- http/nodes.inc.php
+++ http/nodes.inc.php
@@ -78,12 +78,19 @@ function hide_password($str, $password) {
     return str_replace($password, "********", $str);
 }
 
+function checked_mysql_query($sql) {
+    mysql_query($sql);
+    if (mysql_error()) {
+        report_sys_admin("Mysql query error: ".mysql_error()."\nSQL:\n$sql");
+    }
+}
+
 function ssh_log_before($nodeno, $action, $cmd, $time, $password_to_hide) {
     if ($password_to_hide) {
         $cmd = hide_password($cmd, $password_to_hide);
     }
     $cmd = "INSERT INTO ssh_log SET `nodeno`='$nodeno', `action`='".addslashes($action)."', `cmd`='".addslashes($cmd)."', `log_time`='$time'";
-    mysql_query($cmd);
+    checked_mysql_query($cmd);
     if (mysql_affected_rows() != 1)
         report_sys_admin("Failed to save ssh log:\n$cmd");
 }
