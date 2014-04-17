185a3596f4554420a281bb6214cd4262008a7eee
Bojie Li <bojieli@gmail.com>
2014-04-17 16:06:05 +0800

    add -y to yum localinstall in vz init scripts

--- conf/vz-pkg-mirror/fedora-20-x86_64/setup.sh
+++ conf/vz-pkg-mirror/fedora-20-x86_64/setup.sh
@@ -2,4 +2,4 @@
 cat fedora.repo | vzctl exec $id "cat - > /etc/yum.repos.d/fedora.repo"
 cat fedora-updates.repo | vzctl exec $id "cat - > /etc/yum.repos.d/fedora-updates.repo"
 vzctl exec $id "yum makecache"
-vzctl exec $id "yum localinstall --nogpgcheck http://mirrors.ustc.edu.cn/fedora/rpmfusion/free/fedora/rpmfusion-free-release-stable.noarch.rpm http://mirrors.ustc.edu.cn/fedora/rpmfusion/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm"
+vzctl exec $id "yum localinstall -y --nogpgcheck http://mirrors.ustc.edu.cn/fedora/rpmfusion/free/fedora/rpmfusion-free-release-stable.noarch.rpm http://mirrors.ustc.edu.cn/fedora/rpmfusion/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm"
