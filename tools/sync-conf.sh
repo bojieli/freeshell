d764934754661602c2c38430a70ce7ba0d760bf5
Bojie Li <bojieli@gmail.com>
2014-05-02 00:32:13 +0800

    use one-line rsync to upload files without TMPDIR

--- tools/sync-conf.sh
+++ tools/sync-conf.sh
@@ -2,17 +2,12 @@
 
 cd `dirname $0`
 CONFDIR="../conf"
-TMP_DIR=/home/`whoami`/freeshell-scripts
 TARGET_DIR=/home/freeshell/scripts
 
-echo "Preparing for upload..."
-parallel-ssh -h pssh-hosts -P "mkdir -p $TMP_DIR"
-echo "Uploading files to tmpdir..."
+echo "Uploading files..."
 while read host; do
-    rsync -a --delete $CONFDIR/ $host:$TMP_DIR/
+    rsync -a -e "ssh" --rsync-path="sudo rsync" $CONFDIR/ $host:$TARGET_DIR/
 done <pssh-hosts
-echo "Syncing files to config dir..."
-parallel-ssh -h pssh-hosts -P "sudo rsync -a --delete $TMP_DIR/ $TARGET_DIR/"
 
 echo "Updating configs..."
 parallel-ssh -h pssh-hosts -P "sudo /home/freeshell/scripts/update-conf.sh"
