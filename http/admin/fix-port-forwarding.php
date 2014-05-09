68da30649678aff0ce08ce67240723c6084ed2e8
Bojie Li <bojieli@gmail.com>
2014-05-10 03:05:48 +0800

    add user-defined port forwarding in admin tool

--- http/admin/fix-port-forwarding.php
+++ http/admin/fix-port-forwarding.php
@@ -10,4 +10,8 @@ $rs = checked_mysql_query("SELECT id, nodeno FROM shellinfo WHERE isactive=1");
 while ($row = mysql_fetch_array($rs)) {
     add_ssh_port_forwarding($row['id'], $row['nodeno']);
 }
+$rs = checked_mysql_query("SELECT shellinfo.id, nodeno, public_endpoint, private_endpoint, protocol FROM endpoint, shellinfo WHERE endpoint.id = shellinfo.id");
+while ($row = mysql_fetch_array($rs)) {
+    add_endpoint($row['id'], $row['nodeno'], $row['public_endpoint'], $row['private_endpoint'], $row['protocol']);
+}
 echo "Done.";
