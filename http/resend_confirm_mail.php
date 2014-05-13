f169748ebb552261f41f1911029f6bda6eaa1977
Yifan Gao <git@gaoyifan.com>
2014-05-14 03:08:06 +0800

    fix Broken Access Control bug

--- http/resend_confirm_mail.php
+++ http/resend_confirm_mail.php
@@ -4,22 +4,22 @@ include_once "db.php";
 include_once "nodes.inc.php";
 include_once "admin.inc.php";
 
-if (!isset($_SESSION['appid']) || !is_numeric($_SESSION['appid']))
+if (!isset($_SESSION['appid_to_active']) || !is_numeric($_SESSION['appid_to_active']))
     include "logout.php";
-$rs = checked_mysql_query("SELECT token FROM shellinfo WHERE `id`='". $_SESSION['appid']. "'");
+$rs = checked_mysql_query("SELECT token FROM shellinfo WHERE `id`='". $_SESSION['appid_to_active']. "'");
 $token = mysql_result($rs,0);
 if ($token == "")
     include "logout.php";
 session_write_close();
 
-send_activate_mail($_SESSION['email'], $_SESSION['appid'], $token);
+send_activate_mail($_SESSION['email_to_active'], $_SESSION['appid_to_active'], $token);
 ?>
 <div id="wrapper">
 <div id="regtitle">
         	<h1>Confirmation Mail Resent</h1>
         	<div id="progbar">
             </div>
-<p>We have resent you (<?=$_SESSION['email']?>) an confirmation mail.</p>
+<p>We have resent you (<?=$_SESSION['email_to_active']?>) an confirmation mail.</p>
 <p>Please activate your account by clicking the link inside the mail.</p>
 </div>
 <div id="regbutton" onclick="javascript:document.location.href='http://email.ustc.edu.cn'">
