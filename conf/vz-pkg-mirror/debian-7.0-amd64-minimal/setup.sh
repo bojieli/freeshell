7130e5cb23980a20d7e653e94f8e2a9e70e7c097
Bojie Li <bojieli@gmail.com>
2014-07-28 19:39:06 +0800

    fix libssl1.0.0 in debian

--- conf/vz-pkg-mirror/debian-7.0-amd64-minimal/setup.sh
+++ conf/vz-pkg-mirror/debian-7.0-amd64-minimal/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 cat sources.list | vzctl exec $id "cat - > /etc/apt/sources.list"
+vzctl exec $id "cat - >>/var/cache/debconf/config.dat" <<EOF
+Name: libraries/restart-without-asking
+Template: libraries/restart-without-asking
+Value: true
+Owners: libssl1.0.0
+Flags: seen
+EOF
 vzctl exec $id "timeout 60 apt-get update"
