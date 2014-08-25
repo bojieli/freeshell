18b5768aca88431005f77db88c102a5545dc8625
Bojie Li <bojieli@gmail.com>
2014-08-25 11:32:39 +0800

    only allow localhost to see shellmax

--- http/shellmax.php
+++ http/shellmax.php
@@ -1,4 +1,7 @@
 <?php
+if ($_SERVER['REMOTE_ADDR'] != $_SERVER['SERVER_ADDR'] && $_SERVER['REMOTE_ADDR'] != '127.0.0.1')
+    exit();
+
 include_once "db.php";
 $shellmax = mysql_result(checked_mysql_query("SELECT MAX(id) FROM shellinfo"), 0);
 echo $shellmax;
