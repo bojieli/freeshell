26170c681ad0cf3da6f9f79c48bf82e2aadbeb75
Bojie Li <bojieli@gmail.com>
2014-11-13 23:34:00 +0800

    fix update-forwarding script
    the original script clears all rules

--- http/admin/update-forwarding.php
+++ http/admin/update-forwarding.php
@@ -1,4 +1,4 @@
 <?php
-include_once '../port-forwarding.inc.php';
-$fwd = new PortForwarding();
-echo $fwd->commit();
+include_once '../db.php';
+include_once '../nodes.inc.php';
+echo update_port_forwarding();
