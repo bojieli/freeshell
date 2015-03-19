afdf1e30169f6661584a90c628f2d6093951dcbc
Bojie Li <bojieli@gmail.com>
2015-03-19 17:11:44 +0800

    do not send alert email if SSH failed due to connection timeout

--- http/nodes.inc.php
+++ http/nodes.inc.php
@@ -25,7 +25,11 @@ function local_exec($cmd) {
     $elapsed_time = microtime(true) - $start_time;
     $output = implode("\n", $output);
     if ($errno != 0) {
-        report_sys_admin("local sudo failed with status $errno\nSTART TIME: ".date("Y-m-d H:i:s", (int)$start_time)."\nELAPSED TIME: $elapsed_time\nFULL COMMAND:\n$cmd\nOUTPUT:\n$output\n");
+        if ($errno == 255 && $elapsed_time >= 3) {
+            // SSH failed due to connection timeout, keep slient to avoid alert email storm
+        } else {
+            report_sys_admin("local sudo failed with status $errno\nSTART TIME: ".date("Y-m-d H:i:s", (int)$start_time)."\nELAPSED TIME: $elapsed_time\nFULL COMMAND:\n$cmd\nOUTPUT:\n$output\n");
+        }
     }
     return array($errno, $output);
 }
